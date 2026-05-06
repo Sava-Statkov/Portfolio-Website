28d7cb4bbdb775a3951990e548a36064884751c3
Sava-Statkov <savastatkov1@gmail.com>
2026-05-06 04:32:17 +0300

    Added vercel data analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
+import { Analytics } from "@vercel/analytics/next"
 
 import { ThemeProvider } from "@/components/providers/theme-provider";
 
@@ -40,6 +41,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
